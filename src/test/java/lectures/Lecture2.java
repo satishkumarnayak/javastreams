39ecf25759a3d31b76ea94bb11e5d031cce1ac3f
Satish Nayak <satish_nayak@epam.com>
2021-04-15 00:11:04 +0530

    min max

--- src/test/java/lectures/Lecture2.java
+++ src/test/java/lectures/Lecture2.java
@@ -8,22 +8,33 @@ import org.junit.Test;
 
 public class Lecture2 {
 
-  @Test
+ // @Test
   public void range() throws Exception {
+
+    for (int i = 0; i < 10; i++) {
+      System.out.println(i);
+    }
+    
+    IntStream.range(0, 10).forEach(System.out::println);
+    IntStream.rangeClosed(0, 10).forEach(System.out::println);
 
   }
 
   @Test
   public void rangeIteratingLists() throws Exception {
     List<Person> people = MockData.getPeople();
+    IntStream.range(0, people.size())
+              .forEach(index -> {
+                Person p = people.get(index);
+                System.out.println(p);
+              });
 
+    people.stream().forEach(System.out::println);
   }
 
-  @Test
+ // @Test
   public void intStreamIterate() throws Exception {
-      IntStream.iterate(0, operand -> operand + 1)
-          .filter(number -> number % 2 == 0)
-          .limit(20)
-          .forEach(System.out::println);
+    IntStream.iterate(0, operand -> operand + 1).filter(number -> number % 2 == 0).limit(20)
+        .forEach(System.out::println);
   }
 }
--- src/test/java/lectures/Lecture3.java
+++ src/test/java/lectures/Lecture3.java
@@ -5,19 +5,40 @@ import static org.assertj.core.api.Assertions.assertThat;
 import com.google.common.collect.ImmutableList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Optional;
 import org.junit.Test;
 
 public class Lecture3 {
 
   @Test
   public void min() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    final List<Integer> numbers = ImmutableList.of(1, 1, 1, 2);
+
+    Optional<Integer> min = numbers.stream().min((num1, num2) -> {
+      if (num1 == num2) {
+        return 0;
+      } else if (num1 > num2) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+
+    Optional<Integer> min2 = numbers.stream().min((num1, num2) -> {
+      return num1 > num2 ? 1 : -1;
+    });
+
+
+    System.out.println(min.get());
+    System.out.println(min2.get());
 
   }
 
   @Test
   public void max() throws Exception {
-    final List<Integer> numbers = ImmutableList.of(1, 2, 3, 100, 23, 93, 99);
+    final List<Integer> numbers = ImmutableList.of(1, 2, 3, 101, 23, 93, 99);
 
+    Optional<Integer> max = numbers.stream().max(Comparator.naturalOrder());
+    System.out.println(max.get());
   }
 }
